4786eadad8a1d25cbe78d83f43e24c61869d1214
Anna Kristensen <annaokristensen@gmail.com>
2022-05-13 16:21:15 +0200

    Add project files.

---
+++ DataLayer/LokalDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using DTO;
+
+namespace DataLayer
+{
+    public class LokalDatabase
+    {
+        public EKG GetEKG()
+        {
+            return new EKG(new List<double>(),12,new Patient("1111111","Ida"),DateTime.Now);
+        }
+    }
+}
